bfaf280ef79c9dc1ea62f1e531ce7c65d3957476
Vinicius Botelho <viniciusoficialbotelho@outlook.com>
2022-07-13 10:52:00 -0300

    Method POST and GET User

--- src/main/java/br/com/springboot/model/User.java
+++ src/main/java/br/com/springboot/model/User.java
@@ -2,14 +2,16 @@ package br.com.springboot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private Long id;
     private String name;
-    private String userName;
+    private String username;
 }
